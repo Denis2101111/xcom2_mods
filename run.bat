dbabb1ac02f0fe1caa5952d4f8dfdcf7da6e5efd
denisstepanov21011990 <33015809+denisstepanov21011990@users.noreply.github.com>
2022-07-02 00:19:08 +0700

    Start

---
+++ run.bat
@@ -0,0 +1,3 @@
+xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.ini" "L:\Games\XCOM 2 Files\INI\" /d /e /c /y
+xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.rus" "L:\Games\XCOM 2 Files\RUS\" /d /e /c /y
+xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.int" "L:\Games\XCOM 2 Files\INT\" /d /e /c /y
