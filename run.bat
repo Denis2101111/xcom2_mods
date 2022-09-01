ec3aa745f8257ec60472c2a7cb1ac8b5e65087f5
denisstepanov21011990 <33015809+denisstepanov21011990@users.noreply.github.com>
2022-09-01 14:39:12 +0700

    01.09.2022 xmods

--- run.bat
+++
@@ -1,3 +0,0 @@
-xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.ini" "L:\Games\XCOM 2 Files\INI\" /d /s /c /y
-xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.rus" "L:\Games\XCOM 2 Files\RUS\" /d /s /c /y
-xcopy "L:\SteamLibrary\steamapps\workshop\content\268500\*.int" "L:\Games\XCOM 2 Files\INT\" /d /s /c /y
